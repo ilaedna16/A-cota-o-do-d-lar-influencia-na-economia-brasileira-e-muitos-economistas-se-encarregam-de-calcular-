46febb4d1fea520edbe805b2fdd56069e397235d
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-16 19:44:27 -0300

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int dolar_atual; 
+    int valor;
+    
+    printf ("digite o valor em real\n"); 
+    scanf ("%d", &valor); 
+    
+    valor= dolar_atual; 
+    
+    
+    printf ("\n o valor a ser convertido em real eh: %d",valor); 
+    
+    return 0;
+}
